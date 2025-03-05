d50f15af10613bff3bf78ff68d2dd89aa3be08ce
Georgiy Zarubin <ggz1@tpu.ru>
2025-03-05 19:42:59 +0700

    created a hierarchy of classes for the transport

--- Tram.cpp
+++ Tram.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Tram.h"
 
-// Конструктор класса автобуса
+// Конструктор класса трамвая
 Tram::Tram(Route& route) : PublicTransport(5, route) {}
 /// @brief Метод для рассчета стоимости поездки
 void Tram::CalculateFare(){
     Transport::_fare = _farePerKilometer * _route.GetKilometers();
 }
-/// @brief Метод для вывода информации об автобусе
+/// @brief Метод для вывода информации об трамвая
 void Tram::PrintTransportInfo() const {
     std::cout << "Трамвай №"<< _route.GetRouteNumber() << " (ID транспорта: " << Transport::_id << ")" << std::endl;
     _route.PrintRoute();
